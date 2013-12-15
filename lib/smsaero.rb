e4e568e2f45002b0290223e3bd0b5b0fbd7d6986
Valentine Zavadsky <saicheg@gmail.com>
2013-12-15 16:34:39 +0300

    keep only ability to send text for now

--- lib/smsaero.rb
+++ lib/smsaero.rb
@@ -9,10 +9,16 @@ class SMSAero
     raise 'User and password should be provided!' unless options[:user] && options[:password]
 
     @user = options[:user]
-    @password_hash = Digest::MD5.digest options[:password]
+    @password_hash = Digest::MD5.hexdigest(options[:password])
+  end
+
+  def send_message(from, to, message)
+    options = {to: to, text: message, from: from}
+    get('send', options)
   end
 
   def method_missing(action, *args, &block)
+
     options = args.extract_options!
     options.reverse_merge! answer: :json
 
@@ -21,4 +27,15 @@ class SMSAero
 
     Net::HTTP.post_form uri, user: @user, password: @password_hash
   end
+
+  protected
+
+  def get(action, options)
+    options = options.merge(user: @user, password: @password_hash, answer: :json)
+    uri = URI(URL % { action: action})
+    uri.query = URI.encode_www_form options
+
+    Net::HTTP.get(uri)
+  end
+
 end
